4090fff92daf49fc22ccd229f1fd5fa8f362cdc6
Oleg <life2boat@gmail.com>
2026-04-26 17:42:27 +0700

    🚀 CI/CD: Activate automated deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/photodoc-ai/',
   resolve: {
     alias: {
       "@": path.resolve(process.cwd(), "./src"),
